d3ff9fe64741e84e2c55092639c1e5517b0ef301
Dzarel Developer <112155072+DzarelDeveloper@users.noreply.github.com>
2024-03-02 23:37:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('nav ul li a:not(:only-child)').click(function(e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        e.stopPropagation();
+    });
+
+    $('html').click(function(){
+        $('.nav-dropdown').hide();
+    })
+    $('#nav-toggle').click(function(){
+        $('nav ul').slideToggle();
+    })
+    $('#nav-toggle').on('click', function(){
+        this.classList.toggle('active');
+    });
+});
